a3d54735cac96a0fedb5eb3a90f5fef115ba4ac3
ki2183 <ki2183@naver.com>
2025-05-06 19:10:57 +0900

    chore: add link

--- src/component/Section/ProjectInfo/ProjectInfo.tsx
+++ src/component/Section/ProjectInfo/ProjectInfo.tsx
@@ -6,6 +6,7 @@ type portfolio = {
   start: string,
   end: string,
   title: string,
+  link: string,
 }
 
 const ProjectInfo = () => {
@@ -14,29 +15,35 @@ const ProjectInfo = () => {
       start: '2025-03',
       end:'2025-04',
       title:'IT-TIME',
+      link:'https://melted-muscle-989.notion.site/IT-TIME-1d189e654db58046aa5fe8439560b8f2',
     },
     {
       start: '2024-12',
       end:'2025-01',
       title:'EZZ',
+      link:'https://melted-muscle-989.notion.site/EZZ-1df89e654db580b7b7aef4dda8fdaae6?pvs=74',
     },
     {
       start: '2024-06',
       end:'2024-05',
       title:'거산테크',
+      link:'https://www.geosantech.kr/',
     },
     {
       start: '2024-04',
       end:'2024-05',
-      title:'PORTFOLIO SITE',
+      title:'OLD PORTFOLIO SITE',
+      link:'https://portfolio-six-lime-42.vercel.app/',
     }, {
       start: '2024-03',
       end:'2024-04',
       title:'MEMO',
+      link:'https://memo-vercel.vercel.app/',
     }, {
       start: '2023-10',
       end:'2023-09',
       title:'한신대학교 시간표 추천',
+      link:'https://resister-course-vercel.vercel.app/',
     },
   ];
   return (
@@ -55,11 +62,12 @@ export default ProjectInfo;
 const Project = ({
   end,
   start,
-  title
+  title,
+  link,
 }:portfolio) => {
 
   return (
-    <a style={{ display : "block" }}>
+    <a href={link} className="project_link" style={{ display : "block" }}>
       <article className="project_card">
           <time dateTime={`${start}/${end}`} className="">
             {DateToText(start, end)}
--- src/component/Wrapper/MainWrapper/mainWrapper.tsx
+++ src/component/Wrapper/MainWrapper/mainWrapper.tsx
@@ -1,13 +1,16 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useRef } from 'react'
 import './mainWrapper.scss';
+import gsap from 'gsap';
 
 interface Props {
     children?: ReactNode;
 }
 
 const MainWrapper = ({ children }: Props)=> {
+  // 추후 애니메이션 도입
+  const ref = useRef<HTMLElement>(null);
   return (
-    <main className='main_container'>
+    <main ref={ref} className='main_container'>
       {children}
     </main>
   )
